2c3590b36a93a20732dbba5005c54656c14db0ce
HardWare68 <47306310+HardWare68@users.noreply.github.com>
2023-04-28 22:18:30 +0000

    update the code

--- Main.java
+++ Main.java
@@ -6,10 +6,9 @@ public class Main {
       
       boolean[] chptList = new boolean[16]; // Valid chapters are 1-15. 1 means include, 0 exclude. Element 0 unused. 
       int includeChpt;
-      int i, j;
       
       // Get the chapter selections
-      for (i = 1; i <= 15; ++i) {
+      for (int i = 1; i <= 15; ++i) {
          includeChpt = scnr.nextInt(); 
          if (includeChpt==1) {
             chptList[i] = true;
@@ -19,6 +18,21 @@ public class Main {
          }
       }
       
-      /* Type your code here. */
+      for(int i=1; i < chptList.length; i++){
+         
+         if(i < 13){
+            /* Lookahead*/
+            if(chptList[i] && chptList[i+1] && chptList[i+2]){
+               System.out.print(i + "-" + (i+2) + " ");
+               i += 2;
+            } else if (chptList[i]){
+               System.out.print(i + " ");
+            }
+         }
+      }
+      
+      /* End with new line*/
+      System.out.println();
+      
    }
 }
